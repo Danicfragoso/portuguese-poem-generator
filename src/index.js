b95366a78f85c3bc3a3d6e99df91b28a3699259a
Daniela <daniela_fragoso@hotmail.com>
2026-09-25 14:58:06 +0100

    Added basic JavaScript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-function searchPoem () {
+function searchPoem (event) {
+event.preventDefault();
+
+new Typewriter("#poem-here", {
+        strings: ["Poem"],
+        autoStart: true,
+        delay: 50,
+        cursor: "",
+    });
+
 
 }
+let poemForm = document.querySelector("#form-poem");
+poemForm.addEventListener("submit", searchPoem);
