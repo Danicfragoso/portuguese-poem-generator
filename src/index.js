f3fb4973486ca68d52b333e7d65b954ad511e910
Daniela <daniela_fragoso@hotmail.com>
2026-09-25 15:02:56 +0100

    Corrections on typerwriter
    Had to make some changes because the string was looping

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 function searchPoem (event) {
 event.preventDefault();
 
-new Typewriter("#poem-here", {
-        strings: ["Poem"],
-        autoStart: true,
+        new Typewriter("#poem-here", {
         delay: 50,
         cursor: "",
-    });
+    })
+    .typeString("Poem")
+    .start(); }
 
 
-}
+
 let poemForm = document.querySelector("#form-poem");
 poemForm.addEventListener("submit", searchPoem);
